4232109eb6c2dd6cb565f778ab3c6fba41f44c53
gift kimani <gift.kimani@students.moringaschool.com>
2023-09-11 20:44:52 +0300

    complete

--- lib/models.py
+++ lib/models.py
@@ -2,6 +2,8 @@ from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
+from connect import session
+
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
@@ -16,6 +18,24 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', backref=backref('company'))
+    devs = relationship("Dev", secondary="freebies", back_populates="companies")
+
+    def give_freebie(self, dev, item_name, value):
+        freebie = Freebie(
+            item_name=item_name, 
+            value=value,
+            company_id = self.id,
+            dev_id = dev.id
+        )
+        session.add(freebie)
+        session.commit()
+
+    @classmethod
+    def oldest_company(cls):
+        company = session.query(cls).order_by(cls.founding_year).first()
+        return company
+
     def __repr__(self):
         return f'<Company {self.name}>'
 
@@ -25,5 +45,41 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebie', backref=backref('dev'))
+    companies = relationship("Company", secondary="freebies", back_populates="devs")
+
+    def give_away(self, dev, freebie):
+        if freebie.dev == self:
+            freebie.dev == dev
+            session.commit()
+        
+
+    @classmethod
+    def received_one(cls, item_name):
+        dev = session.query(cls).filter_by(name=item_name).first()
+        if dev:
+            return True
+        else:
+            return False
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    # company = relationship('Company', back_populates='freebies')
+    # dev = relationship('Dev', back_populates='freebies')
+
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
+
+    def __repr__(self):
+
+        return f'Freebie(id={self.id})'
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,66 @@
 #!/usr/bin/env python3
 
-# Script goes here!
+#!/usr/bin/env python3
+
+from faker import Faker
+import random
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Company, Dev, Freebie
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///freebies.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Company).delete()
+    session.query(Dev).delete()
+    session.query(Freebie).delete()
+
+    fake = Faker()
+
+    years = [2000, 2001, 2002, 2003, 2004, 2005]
+
+    companies = []
+    for i in range(10):
+        company = Company(
+            name=fake.unique.name(),
+            founding_year=random.choice(years),
+        )
+
+        # add and commit individually to get IDs back
+        session.add(company)
+        session.commit()
+
+        companies.append(company)
+
+    devs = []
+    for i in range(10):
+        dev = Dev(
+            name=fake.unique.name(),
+        )
+
+        # add and commit individually to get IDs back
+        session.add(dev)
+        session.commit()
+
+        devs.append(dev)
+
+    freebies = []
+    for company in companies:
+        for i in range(random.randint(1,5)):
+            
+            freebie = Freebie(
+                item_name= fake.unique.name(),
+                value = random.randint(5, 100),
+                company_id=company.id,
+                dev_id = devs[i].id
+            )
+
+            freebies.append(freebie)
+
+    session.bulk_save_objects(freebies)
+    session.commit()
+    session.close()
